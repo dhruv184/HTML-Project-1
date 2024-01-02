d96e543e447384b389d08c0f9ccd928fb4be2263
Dhruv Patel <dhruvpatel@dhruvs-air.home>
2024-01-02 14:44:13 -0500

    added HTML project-1

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+    $(".panel-all").click(function () {
+        $(".panel-ops").toggle();
+    });
+
+    $(".hero-message a").click(function () {
+        window.open("https://www.amazon.in", "_blank");
+    });
+});
+
+
